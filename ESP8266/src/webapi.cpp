c4cae3397104b0b0736af25e00617957dab2000e
Jurjan Brust <jurjanbrust@hotmail.com>
2022-06-27 21:24:53 +0200

    try catch on webapi call

--- ESP8266/src/webapi.cpp
+++ ESP8266/src/webapi.cpp
@@ -27,48 +27,54 @@ WEBAPI::WEBAPI(VFD& vfd) {
 
 void WEBAPI::update() {
 
-  WiFiClientSecure httpsClient;
-  httpsClient.setInsecure();
-  httpsClient.setTimeout(15000); // 15 Seconds
-  delay(1000);
+  try {
+    WiFiClientSecure httpsClient;
+    httpsClient.setInsecure();
+    httpsClient.setTimeout(15000); // 15 Seconds
+    delay(1000);
 
-  int retry=0;
-  while((!httpsClient.connect(_host, 443)) && (retry < 20)){
-      delay(100);
-      DEBUG_PRINT(".");
-      retry++;
-  }
-
-  DEBUG_PRINT("requesting URL: ");
-  DEBUG_PRINT(_host + _feed);
-  httpsClient.print(String("GET ") + _feed + " HTTP/1.1\r\n" +
-               "Host: " + _host + "\r\n" +
-               "Connection: close\r\n\r\n");
-
-
-  DEBUG_PRINT("headers:");
-  while (httpsClient.connected()) {
-    _json = httpsClient.readStringUntil('\n');
-    DEBUG_PRINT(_json);
-    if (_json == "\r") {
-      break;
+    int retry=0;
+    while((!httpsClient.connect(_host, 443)) && (retry < 20)){
+        delay(100);
+        DEBUG_PRINT(".");
+        retry++;
     }
-  }
-  
-  httpsClient.readStringUntil('\n'); // The API sends an extra line with just a number. This breaks the JSON parsing, hence an extra read
-  while(httpsClient.connected()){
-    _json = httpsClient.readString();
-    DEBUG_PRINT(_json);
-  }
 
-  _doc.clear(); // free up memory; see solution 2: https://arduinojson.org/v6/issues/memory-leak
-  DeserializationError err = deserializeJson(_doc,_json);
-  if (err) {
+    DEBUG_PRINT("requesting URL: ");
+    DEBUG_PRINT(_host + _feed);
+    httpsClient.print(String("GET ") + _feed + " HTTP/1.1\r\n" +
+                "Host: " + _host + "\r\n" +
+                "Connection: close\r\n\r\n");
+
+
+    DEBUG_PRINT("headers:");
+    while (httpsClient.connected()) {
+      _json = httpsClient.readStringUntil('\n');
+      DEBUG_PRINT(_json);
+      if (_json == "\r") {
+        break;
+      }
+    }
+    
+    httpsClient.readStringUntil('\n'); // The API sends an extra line with just a number. This breaks the JSON parsing, hence an extra read
+    while(httpsClient.connected()){
+      _json = httpsClient.readString();
+      DEBUG_PRINT(_json);
+    }
+
+    _doc.clear(); // free up memory; see solution 2: https://arduinojson.org/v6/issues/memory-leak
+    DeserializationError err = deserializeJson(_doc,_json);
+    if (err) {
+      _vfd->clear();
+      _vfd->typeWriteHorizontal("deserializeJson failed");
+      delay(5000);
+    }
+    DEBUG_PRINT("size: " + _doc.size());
+  } catch (std::exception& e) {
     _vfd->clear();
-    _vfd->typeWriteHorizontal("deserializeJson failed");
+    _vfd->typeWriteHorizontal("Exception: " + String(e.what()));
     delay(5000);
   }
-  DEBUG_PRINT("size: " + _doc.size());
 }
 
 void WEBAPI::start() {
