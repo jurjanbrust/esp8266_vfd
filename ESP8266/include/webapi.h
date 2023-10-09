ea7581540c39c9423399fdfd21876ede3aeacb54
Jurjan Brust <jurjanbrust@hotmail.com>
2023-10-09 18:36:41 +0200

    better json parser

--- ESP8266/include/webapi.h
+++ ESP8266/include/webapi.h
@@ -22,27 +22,4 @@ class WEBAPI
     String _feed;
     String _host;
     boolean _forecast;
-
-    enum DisplayMode {
-      Normal = 0,
-      FadeOut = 1,
-      FadeIn = 2,
-      Scroll = 3,
-      KnightRider = 4,
-      KnightRider2 = 5,
-      KnightRider3 = 6,
-      VerticalScroll = 7,
-      HorizontalScroll = 8,
-      ClearScreen = 9,
-      SetBrightness0 = 10,
-      SetBrightness1 = 11,
-      SetBrightness2 = 12,
-      SetBrightness3 = 13,
-      SetBrightness4 = 14,
-      SetBrightness5 = 15,
-      SetBrightness6 = 16,
-      SetBrightness7 = 17,
-      FadeLeftToRight = 18,
-      FadeRightToLeft = 19
-    };
 };
--- ESP8266/src/webapi.cpp
+++ ESP8266/src/webapi.cpp
@@ -16,7 +16,18 @@
   #endif
 #endif
 
-StaticJsonDocument<5000> _doc;  // 10 kb max size
+StaticJsonDocument<5000> _doc;
+
+String extractJsonContent(const String &input) {
+    int start = input.indexOf('[');
+    int end = input.lastIndexOf(']');
+
+    if (start != -1 && end != -1 && start < end) {
+        return input.substring(start, end + 1);
+    } else {
+        return "";
+    }
+}
 
 WEBAPI::WEBAPI(VFD& vfd) {
 
@@ -28,70 +39,85 @@ WEBAPI::WEBAPI(VFD& vfd) {
 }
 
 void WEBAPI::update() {
+
   try {
-    int retry = 0;
-    while ((!httpsClient.connect(_host, 443)) && (retry < 20)) {
-      delay(100);
-      DEBUG_PRINT(".");
-      retry++;
+    int retry=0;
+    while((!httpsClient.connect(_host, 443)) && (retry < 20)){
+        delay(100);
+        DEBUG_PRINT(".");
+        retry++;
     }
 
     DEBUG_PRINT("requesting URL: ");
     DEBUG_PRINT(_host + _feed);
     httpsClient.print(String("GET ") + _feed + " HTTP/1.1\r\n" +
-                      "Host: " + _host + "\r\n" +
-                      "Connection: close\r\n\r\n");
+                "Host: " + _host + "\r\n" +
+                "Connection: close\r\n\r\n");
+
 
     DEBUG_PRINT("headers:");
-    
-    size_t contentLength = 0;
-
     while (httpsClient.connected()) {
       _json = httpsClient.readStringUntil('\n');
       DEBUG_PRINT(_json);
-
-      if (_json.startsWith("Content-Length: ")) {
-        contentLength = _json.substring(16).toInt();
-      }
-
       if (_json == "\r") {
         httpsClient.readStringUntil('\n'); // The API sends an extra line with just a number. This breaks the JSON parsing, hence an extra read
         break;
       }
     }
 
-    while (httpsClient.available()) {
-      _json += httpsClient.readString();
+    DEBUG_PRINT("body:");
+    while(httpsClient.available()){
+      _json = httpsClient.readString();
+      DEBUG_PRINT(_json);
     }
-
-    // Dynamically allocate memory for the JSON document
-    DynamicJsonDocument doc(contentLength);
-    DeserializationError err = deserializeJson(doc, _json);
-
+    DEBUG_PRINT("end body");
+    String extractedJson = extractJsonContent(_json);
+    
+    _doc.clear(); // clears the reserved memory; see solution 2: https://arduinojson.org/v6/issues/memory-leak
+    DeserializationError err = deserializeJson(_doc,extractedJson);
     if (err) {
       _vfd->clear();
       _vfd->typeWriteHorizontal("deserializeJson failed: " + String(err.c_str()));
       delay(5000);
     }
-
-    // Clear the reserved memory
-    _json = "";
+    
     httpsClient.stop();
 
-    DEBUG_PRINT("size: " + doc.size());
+    DEBUG_PRINT("size: " + _doc.size());
 
-  } catch (std::exception &e) {
+  } catch (std::exception& e) {
     _vfd->clear();
     _vfd->typeWriteHorizontal("Exception: " + String(e.what()));
     delay(5000);
   }
 }
 
-
 void WEBAPI::start() {
 
-  DisplayMode displaymode = Normal;  // Use the enum type
+  enum mode {
+            Normal,               // 0
+            FadeOut,              // 1
+            FadeIn,               // 2
+            Scroll,               // 3
+            KnightRider,          // 4
+            KnightRider2,         // 5
+            KnightRider3,         // 6
+            VerticalScroll,       // 7
+            HorizontalScroll,     // 8
+            ClearScreen,          // 9
+            SetBrightness0,       // 10
+            SetBrightness1,       // 11
+            SetBrightness2,       // 12
+            SetBrightness3,       // 13
+            SetBrightness4,       // 14
+            SetBrightness5,       // 15
+            SetBrightness6,       // 16
+            SetBrightness7,       // 17
+            FadeLeftToRight,      // 18
+            FadeRightToLeft,      // 19
+  };
 
+  int displaymode = 0;
   String line1;
   String line2;
 
@@ -100,7 +126,7 @@ void WEBAPI::start() {
 
   for(unsigned int i=0; i<_doc.size();i++) {
     
-    displaymode = static_cast<DisplayMode>(_doc[i]["displayMode"].as<int>());
+    displaymode = _doc[i]["displayMode"].as<int>();
     line1 = _doc[i]["line1"].as<String>();
     line2 = _doc[i]["line2"].as<String>();
 
